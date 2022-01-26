58ffd51a6e479ac1356df4a3d9f5fb4249b503a6
septianwijaya45 <septianwijaya170845@gmail.com>
2022-01-26 19:35:30 +0700

    First Commit & Edit App.js

---
+++ App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.textTitle}>GAMEON</Text>
+      </View>
+      <TouchableOpacity style={styles.begin}>
+        <Text style={styles.textBegin}>Let's Begin</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  textTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#20315f',
+  },
+  begin: {
+    backgroundColor: '#AD40AF',
+    padding: 20,
+    width: '90%',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textBegin: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
+
+export default App;
